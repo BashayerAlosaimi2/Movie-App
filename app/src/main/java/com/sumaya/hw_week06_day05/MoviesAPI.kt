80c48fd02b5e9ca09b7c5d8d0a6d6e553117129b
Bashayer Alosaimi <bashayer.alosaimi37@gmail.com>
2021-11-06 20:50:53 +0300

    done

--- app/src/main/java/com/sumaya/hw_week06_day05/MoviesAPI.kt
+++ app/src/main/java/com/sumaya/hw_week06_day05/MoviesAPI.kt
@@ -6,9 +6,9 @@ import retrofit2.http.Query
 
 interface MoviesAPI {
     //we should implement here the four CRUD functions for REST
-    @GET("https://api.themoviedb.org/3/movie/popular?api_key=11e918a3449651695c0e1e918df27ade&language=en-US&page=1")
+    @GET("https://api.themoviedb.org/3/movie/popular?api_key=3018c5d682f9a8f4bae801594f89d1e9&language=en-US&page=1")
     suspend fun fetchMovies(): MoviesData
 
-    @GET("https://api.themoviedb.org/3/search/movie?api_key=11e918a3449651695c0e1e918df27ade&language=en-US&page=1&include_adult=false")
+    @GET("https://api.themoviedb.org/3/search/movie?api_key=3018c5d682f9a8f4bae801594f89d1e9&language=en-US&page=1&include_adult=false")
     suspend fun searchMovies(@Query("query") searchKey:String ): MoviesData
 }
